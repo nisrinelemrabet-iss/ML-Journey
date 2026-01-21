93a9ebf5efca0d3ef0c451dc916b1e165b09cb96
nisrinelemrabet-iss <nisrinelemrabet@gmail.com>
2026-01-21 00:26:27 +0000

    Python basics notebook for ML

---
+++ 01-python-basics/variables_and_types.py
@@ -0,0 +1,11 @@
+# Variables & Types – ML basics
+
+age = 21
+height = 1.68
+name = "Nissrine"
+is_engineering_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Engineering student:", is_engineering_student)
